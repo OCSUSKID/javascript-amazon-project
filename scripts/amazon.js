ca37581f4ba98f81dbf14fe84e0163bac3504e20
success <sokoroafor744@gmail.com>
2026-06-29 13:01:39 +0100

    make dropdown selector interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach(product => {
       </div>
 
       <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
           <option selected value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -62,6 +62,9 @@ document.querySelectorAll('.js-add-to-cart').forEach(button => {
   button.addEventListener('click', () => {
     const productId = button.dataset.productId;
 
+    const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+    const quantity = Number(quantitySelector.value);
+
     let matchingItem;
 
     cart.forEach(item => {
@@ -76,7 +79,7 @@ document.querySelectorAll('.js-add-to-cart').forEach(button => {
       cart.push({
         // productId: productId,
         productId,
-        quantity: 1
+        quantity: quantity
       });
     }
 
