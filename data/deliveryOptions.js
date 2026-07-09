87bc32f5958d50cd5a9db76ec3034ab189060a55
success <sokoroafor744@gmail.com>
2026-07-09 16:01:54 +0100

    updated the delivery date, when calculating the delivery date, skip weekends (all saturdays and sundays)

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -1,4 +1,5 @@
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
+
 export const deliveryOptions = [
   {
     id: '1',
@@ -29,12 +30,33 @@ export function getDeliveryOption(deliveryOptionId) {
   return deliveryOption || deliveryOption[0];
 }
 
+function isWeekend(date) {
+  const dayOfWeek = date.format('dddd');
+  return dayOfWeek === 'Saturday' || dayOfWeek === 'Sunday';
+}
+
+// Time complexity: O(d) where d = deliveryOption.deliveryDays.
+// The function increments the date one day at a time and checks weekends,
+// so runtime scales linearly with the number of delivery days.
+
 export function calculateDeliveryDate(deliveryOption) {
-  const today = dayjs();
-  const deliveryDate = today.add(
-    deliveryOption.deliveryDays,
-    'days'
-  );
+  // got the number of days from the deliveryOption object
+  let remainingDays = deliveryOption.deliveryDays;
+
+  // create a dayjs object for the current date
+  let deliveryDate = dayjs();
+
+  // loop until we have added the required number of delivery days
+  while (remainingDays > 0) {
+    // add one day to the deliveryDate
+    deliveryDate = deliveryDate.add(1, 'days');
+
+    // if the new deliveryDate is not a weekend, decrement remainingDays
+    if (!isWeekend(deliveryDate)) {
+      remainingDays--;
+    }
+  }
+
   const dateString = deliveryDate.format(
     'dddd, MMMM, D'
   );
--- scripts/checkWeekend.js
+++ scripts/checkWeekend.js
@@ -1,10 +1,4 @@
 export default function isWeekend(date) {
   const dayOfWeek = date.format('dddd');
-  if (dayOfWeek === 'Saturday' || dayOfWeek === 'Sunday') {
-    console.log('It is a weekend');
-  } else {
-    console.log('It is a weekday');
-  }
-
-  return dayOfWeek;
+  return dayOfWeek === 'Saturday' || dayOfWeek === 'Sunday';
 }
