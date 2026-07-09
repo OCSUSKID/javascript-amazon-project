458025e0869bdb183b998921e04e79bd7691ab69
success <sokoroafor744@gmail.com>
2026-07-09 14:04:40 +0100

    use MVC instead of DOM to re-render and update the checkout header page when deleting a product from cart

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -11,6 +11,7 @@ import { hello } from "https://unpkg.com/supersimpledev@1.0.1/hello.esm.js";
 import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
 import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
 import { renderPaymentSummary } from "./paymentSummary.js";
+import { renderCheckoutHeader } from "./checkoutHeader.js";
 
 export function renderOrderSummary() {
 
@@ -132,6 +133,7 @@ export function renderOrderSummary() {
         const { productId } = link.dataset;
         removeFromCart(productId);
 
+        renderCheckoutHeader();
         renderOrderSummary();
 
         updateCartQuantity();
